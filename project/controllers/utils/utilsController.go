9822dca9572f9647e66a02c72c345b2ad37f33a8
Lxb921006 <120332269@qq.com>
2023-02-10 15:02:47 +0800

    验证是否是sql文件

--- project/controllers/utils/utilsController.go
+++ project/controllers/utils/utilsController.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"net/http"
 	"path/filepath"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Upload(ctx *gin.Context) {
-
 	file, err := ctx.FormFile("file")
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{
@@ -17,14 +17,20 @@ func Upload(ctx *gin.Context) {
 		})
 		return
 	}
+
 	rdata := make(map[string]interface{})
 
-	if err := ctx.SaveUploadedFile(file, filepath.Join(SaveSqlPath, file.Filename)); err != nil {
-		rdata["msg"] = err.Error()
-		rdata["code"] = 10001
+	if strings.HasSuffix(file.Filename, ".sql") {
+		if err := ctx.SaveUploadedFile(file, filepath.Join(SaveSqlPath, file.Filename)); err != nil {
+			rdata["msg"] = err.Error()
+			rdata["code"] = 10001
+		} else {
+			rdata["msg"] = fmt.Sprintf("%s上传成功", file.Filename)
+			rdata["code"] = 10000
+		}
 	} else {
-		rdata["msg"] = fmt.Sprintf("%s上传成功", file.Filename)
-		rdata["code"] = 10000
+		rdata["msg"] = "只能上传sql文件"
+		rdata["code"] = 10002
 	}
 
 	ctx.JSON(http.StatusOK, rdata)
